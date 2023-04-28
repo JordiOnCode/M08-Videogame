fa1c3ecedd85ca1694407a087e18d947f629a981
Jordi Montero <jordimonterov32@gmail.com>
2023-04-28 12:52:13 +0200

    Movimiento jugador (WIP)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,24 +4,76 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float walkSpeed;
+    private float moveInput;
+    public bool isGrounded;
     private Rigidbody2D rb;
+    public LayerMask groundMask;
+
+    public PhysicsMaterial2D bounceMat, normalMat;
+    public bool canJump = true;
+    public float jumpValue = 0;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        moveInput = Input.GetAxisRaw("Horizontal");
 
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump"))
+        if (jumpValue == 0.0f && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 20f);
+            rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
         }
-    }    
+
+        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f), new Vector2(0.9f, 0.4f), 0f, groundMask);
+
+        if (jumpValue > 0)
+        {
+            rb.sharedMaterial = bounceMat;
+        }
+        else
+        {
+            rb.sharedMaterial = normalMat;
+        }
+
+        if (Input.GetKey("space") && isGrounded && canJump)
+        {
+            jumpValue += 0.1f;
+        }
+
+        if (Input.GetKeyDown("space") && isGrounded && canJump)
+        {
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+        }
+
+        if (jumpValue >= 20f && isGrounded)
+        {
+            float tempx = moveInput * walkSpeed;
+            float tempy = jumpValue;
+            rb.velocity = new Vector2(tempx, tempy);
+            Invoke("ResetJump", 0.2f);
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(moveInput * walkSpeed, jumpValue);
+                jumpValue = 0.0f;
+            }
+            canJump = true;
+        }
+    }
+
+    void ResetJump()
+    {
+        canJump = false;
+        jumpValue = 0;
+    }
 }
