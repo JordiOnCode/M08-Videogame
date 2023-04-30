7830465443d7f805755bc5b59b6543a325747024
Jordi Montero <jordimonterov32@gmail.com>
2023-04-30 13:45:57 +0200

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,86 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float maxJumpHeight = 10f;
+    public float jumpChargeSpeed = 5f;
+    public LayerMask groundLayer;
 
-    public float walkSpeed;
-    private float moveInput;
-    public bool isGrounded;
     private Rigidbody2D rb;
-    public LayerMask groundMask;
+    private float jumpCharge;
+    private bool isJumping;
 
-    public PhysicsMaterial2D bounceMat, normalMat;
-    public bool canJump = true;
-    public float jumpValue = 0;
-    public float maxJumpValue = 10.0f; // Nueva variable para limitar la altura máxima del salto
-    public float jumpIncrement = 0.05f; // Nueva variable para controlar la cantidad en la que se incrementa el salto
-
-    // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        float moveX = 0;
 
-        if (jumpValue == 0.0f && isGrounded)
+        if (IsGrounded() && !isJumping)
         {
-            rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+            moveX = Input.GetAxis("Horizontal");
         }
 
-        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f),
-        new Vector2(0.9f, 0.4f), 0f, groundMask);
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
 
-        if (jumpValue > 0)
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.sharedMaterial = bounceMat;
-        }
-        else
-        {
-            rb.sharedMaterial = normalMat;
+            isJumping = true;
+            jumpCharge = 0;
         }
 
-        if (Input.GetKey("space") && isGrounded && canJump)
+        if (Input.GetButton("Jump") && isJumping)
         {
-            if (jumpValue < maxJumpValue) // Limita la altura máxima del salto
-            {
-                jumpValue += jumpIncrement; // Incrementa el salto en una cantidad menor
-            }
+            jumpCharge += Time.deltaTime * jumpChargeSpeed;
+            jumpCharge = Mathf.Min(jumpCharge, maxJumpHeight);
         }
 
-        if (Input.GetKeyDown("space") && isGrounded && canJump)
+        if (Input.GetButtonUp("Jump") && isJumping)
         {
-            rb.velocity = new Vector2(0.0f, rb.velocity.y);
-        }
-
-        if (jumpValue >= maxJumpValue && isGrounded) // Usa la variable maxJumpValue para limitar la altura máxima del salto
-        {
-            float tempx = moveInput * walkSpeed;
-            float tempy = jumpValue;
-            rb.velocity = new Vector2(tempx, tempy);
-            Invoke("ResetJump", 0.2f);
-        }
-
-        if (Input.GetKeyUp("space"))
-        {
-            if (isGrounded)
-            {
-                rb.velocity = new Vector2(moveInput * walkSpeed, jumpValue);
-                jumpValue = 0.0f;
-            }
-            canJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpCharge);
+            isJumping = false;
         }
     }
 
-    void ResetJump()
+    bool IsGrounded()
     {
-        canJump = false;
-        jumpValue = 0;
+        float extraHeight = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(rb.position, Vector2.down, rb.GetComponent<Collider2D>().bounds.extents.y + extraHeight, groundLayer);
+        return raycastHit.collider != null;
     }
 }
-
-
-
 
