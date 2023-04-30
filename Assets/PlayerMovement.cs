2802d335f79bf08249219cd2f2f47f7e84f35fb0
Jordi Montero <jordimonterov32@gmail.com>
2023-04-30 13:51:01 +0200

    Update de choque personaje

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float maxJumpHeight = 10f;
-    public float jumpChargeSpeed = 5f;
+    public float maxJumpHeight = 20f;
+    public float jumpChargeSpeed = 10f;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private float jumpCharge;
     private bool isJumping;
+    private bool isColliding; // Variable para saber si el jugador está colisionando
 
     void Start()
     {
@@ -22,14 +23,14 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveX = 0;
 
-        if (IsGrounded() && !isJumping)
+        if (IsGrounded() && !isJumping && !isColliding) // Solo permitir el movimiento si el jugador no está saltando y no está colisionando
         {
             moveX = Input.GetAxis("Horizontal");
         }
 
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded() && !isColliding)
         {
             isJumping = true;
             jumpCharge = 0;
@@ -52,7 +53,15 @@ public class PlayerMovement : MonoBehaviour
     {
         float extraHeight = 0.1f;
         RaycastHit2D raycastHit = Physics2D.Raycast(rb.position, Vector2.down, rb.GetComponent<Collider2D>().bounds.extents.y + extraHeight, groundLayer);
+        if (raycastHit.collider != null)
+        {
+            isColliding = false; // El jugador ha aterrizado en un bloque, por lo que ya no está colisionando
+        }
         return raycastHit.collider != null;
     }
-}
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isColliding = true; // El jugador está colisionando con un bloque
+    }
+}
