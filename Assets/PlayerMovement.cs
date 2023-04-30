f99013ed7c95edc11b537f4dd3c9bd738a77a75e
Jordi Montero <jordimonterov32@gmail.com>
2023-04-30 12:11:59 +0200

    PlayerMovement JumpKing Update

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     public PhysicsMaterial2D bounceMat, normalMat;
     public bool canJump = true;
     public float jumpValue = 0;
+    public float maxJumpValue = 10.0f; // Nueva variable para limitar la altura máxima del salto
+    public float jumpIncrement = 0.05f; // Nueva variable para controlar la cantidad en la que se incrementa el salto
 
     // Start is called before the first frame update
     private void Start()
@@ -31,7 +33,8 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
         }
 
-        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f), new Vector2(0.9f, 0.4f), 0f, groundMask);
+        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f),
+        new Vector2(0.9f, 0.4f), 0f, groundMask);
 
         if (jumpValue > 0)
         {
@@ -44,7 +47,10 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("space") && isGrounded && canJump)
         {
-            jumpValue += 0.1f;
+            if (jumpValue < maxJumpValue) // Limita la altura máxima del salto
+            {
+                jumpValue += jumpIncrement; // Incrementa el salto en una cantidad menor
+            }
         }
 
         if (Input.GetKeyDown("space") && isGrounded && canJump)
@@ -52,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(0.0f, rb.velocity.y);
         }
 
-        if (jumpValue >= 20f && isGrounded)
+        if (jumpValue >= maxJumpValue && isGrounded) // Usa la variable maxJumpValue para limitar la altura máxima del salto
         {
             float tempx = moveInput * walkSpeed;
             float tempy = jumpValue;
@@ -77,3 +83,4 @@ public class PlayerMovement : MonoBehaviour
         jumpValue = 0;
     }
 }
+
