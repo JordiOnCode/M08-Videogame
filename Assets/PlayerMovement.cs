53ee15dbbb5ff877210e2bbcb62aa936a42a1552
Jordi Montero <jordimonterov32@gmail.com>
2023-05-16 13:21:48 +0200

    Input Manager (DONE) WITHOUT BUTTONS

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+
+public class PlayerMovement : MonoBehaviour, Controls.IPlayerActions
 {
+    // Agrega más botones si los necesitas
+
+
     public float moveSpeed = 6f;
     public float maxJumpHeight = 20f;
     public float jumpChargeSpeed = 30f;
-
-    // Nueva variable para la velocidad máxima de caída.
     public float maxFallSpeed = -15f;
 
     [SerializeField] private LayerMask jumpableGround;
@@ -22,6 +25,8 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        Controls controls = new Controls();
+        controls.Player.SetCallbacks(this);
     }
 
     void Update()
@@ -37,7 +42,6 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
         }
 
-        // Verificar y limitar la velocidad de caída.
         if (rb.velocity.y < maxFallSpeed)
         {
             rb.velocity = new Vector2(rb.velocity.x, maxFallSpeed);
@@ -72,4 +76,44 @@ public class PlayerMovement : MonoBehaviour
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        float moveX = context.ReadValue<Vector2>().x;
+        if (IsGrounded() && !isJumping)
+        {
+            rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        }
+        else if (isJumping)
+        {
+            rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && IsGrounded())
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (context.performed && IsGrounded())
+        {
+            isJumping = true;
+            jumpCharge = 0;
+        }
+
+        if (context.performed && isJumping)
+        {
+            jumpCharge += Time.deltaTime * jumpChargeSpeed;
+            jumpCharge = Mathf.Min(jumpCharge, maxJumpHeight);
+        }
+
+        if (context.canceled && isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpCharge);
+            isJumping = false;
+        }
+    }
+
 }
